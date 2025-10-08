51b61f00a3177ea3e3a8bcd7a9f882ab62f07690
Khizr Abbas <khizer@example.com>
2025-10-08 22:19:24 +0500

    files

--- src/components/Gradientbutton.tsx
+++ src/components/Gradientbutton.tsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+
 type GradientButtonProps = {
   text: string;
   link: string;
-  className: string
+  className?: string;
 };
 
-
-const GradientButton: React.FC<GradientButtonProps> = ({text, link, className=''}) => {
+const GradientButton: React.FC<GradientButtonProps> = ({ text, link, className = '' }) => {
   return (
-    <>
-        <a href={link} className={`btn uppercase font-heading border-2 border-transparent text-center min-w-[205px] px-12 py-2 lg:py-3 rounded-full max-sm:text-lg ${className} `}>{text}</a>
-    </>
-  )
-}
+    <Link 
+      to={link} 
+      className={`btn uppercase font-heading border-2 border-transparent text-center min-w-[205px] px-12 py-2 lg:py-3 rounded-full max-sm:text-lg ${className}`}
+    >
+      {text}
+    </Link>
+  );
+};
 
-export default GradientButton
+export default GradientButton;
